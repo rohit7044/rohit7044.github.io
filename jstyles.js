4871efe63fa8a3a34fed5d7f0d163e201b5e6f1e
ROHIT DAS <rdas.879@gmail.com>
2023-05-23 22:48:40 +0800

    small changes

--- jstyles.js
+++
@@ -1,5 +0,0 @@
-// pointer
-document.addEventListener("mousemove", e => {
-    document.documentElement.style.setProperty("--mouse-x", e.clientX +'px');
-    document.documentElement.style.setProperty("--mouse-y", e.clientY +'px');
-  });
